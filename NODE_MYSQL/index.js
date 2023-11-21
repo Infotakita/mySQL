d7e35b2c362c337dbe7eb893b5e0d50692eae047
EA - Paulo Takita De Assis <paulo_takita@educadventista.org>
2023-11-21 20:14:08 -0300

    Deletando dados do banco de dados e completando o CRUD

--- NODE_MYSQL/index.js
+++ NODE_MYSQL/index.js
@@ -23,6 +23,22 @@ app.use(express.urlencoded({
 app.use(express.json())
 
 // rotas
+app.post("/delete/:id", (request, response) =>{
+    const {id} = request.body
+
+    const sql = `
+        DELETE FROM books
+        WHERE id = $(id)
+    `
+    conn.query(sql,(error) =>{
+        if (error) {
+            return console.log(error)
+
+        }
+
+        response.redirect("/")
+    })
+})
 app.post("/register/save", (request, response) => {
     response.render("register");
 });
